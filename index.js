413f6fbe3a822dd197ff7a2b747667ff10df15ac
donthaveaneme <donthaveaneme@gmail.com>
2024-01-04 15:33:08 +0700

    modified index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
+// Fungsi untuk memeriksa otentikasi
 function checkAuthentication() {
-
-    const isAuthenticated = 
-
+    // Misalnya, cek keberadaan token sesi
+    var isAuthenticated = localStorage.getItem('sessionToken') !== null;
 
     if (!isAuthenticated) {
-        window.location.href = '/login.html';
+        window.location.href = 'https://inababykids.github.io/login'; // Redirect ke halaman login jika tidak terotentikasi
     }
-
 }
 
+// Panggil fungsi saat halaman dimuat
 document.addEventListener('DOMContentLoaded', function() {
     checkAuthentication();
 });
