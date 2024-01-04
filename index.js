4a7f225300f40b0cf9fadf8957eb471692be2dcc
inababykids <144520121+inababykids@users.noreply.github.com>
2024-01-04 15:38:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-// Fungsi untuk memeriksa otentikasi
+
 function checkAuthentication() {
-    // Misalnya, cek keberadaan token sesi
+
     var isAuthenticated = localStorage.getItem('sessionToken') !== null;
 
     if (!isAuthenticated) {
-        window.location.href = 'https://inababykids.github.io/login'; // Redirect ke halaman login jika tidak terotentikasi
+        window.location.href = '/login.html';
     }
 }
 
-// Panggil fungsi saat halaman dimuat
 document.addEventListener('DOMContentLoaded', function() {
     checkAuthentication();
 });
