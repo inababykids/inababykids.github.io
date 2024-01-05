2d9b040d430fce974a83b6b8e3fcc91a6afa94ff
inababykids <144520121+inababykids@users.noreply.github.com>
2024-01-05 17:52:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// Fungsi logout yang perlu di definisikan
 function logout() {
+    // Hapus token sesi dari localStorage
     localStorage.removeItem('sessionToken');
 
+    // Redirect ke halaman login setelah logout
     window.location.href = '/login.html';
 }
 
+// Fungsi untuk memeriksa otentikasi
 function checkAuthentication() {
     var isAuthenticated = localStorage.getItem('sessionToken') !== null;
 
@@ -12,13 +16,16 @@ function checkAuthentication() {
     }
 }
 
+// Event listener untuk tombol logout
 document.addEventListener('DOMContentLoaded', function() {
+    // Memeriksa otentikasi saat halaman dimuat
     checkAuthentication();
 
+    // Menambahkan event listener untuk tombol logout
     var logoutButton = document.getElementById('logoutButton');
     if (logoutButton) {
         logoutButton.addEventListener('click', function() {
-
+            // Memanggil fungsi logout jika tombol logout ditemukan
             logout();
         });
     }
