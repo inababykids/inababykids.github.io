c6abc2a722bc8c7cc229cfbdcfd123a6bdfc2f2e
inababykids <144520121+inababykids@users.noreply.github.com>
2024-01-05 16:12:32 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,45 +1,19 @@
 function validateLogin(event) {
-    event.preventDefault();
+    event.preventDefault(); 
 
-    // Kredensial OAuth
-    const clientId = '793638504770-j76a4gsemn2abce5gev38sujpicsrqd8.apps.googleusercontent.com';
-    const clientSecret = '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';
-    const redirectUri = 'urn:ietf:wg:oauth:2.0:oob';
+    const apiKey = 'AIzaSyBKTs08rp18jacSVRdE1HGpgHAdGyqBXkQ';
     const spreadsheetId = '1F5voYTnm3e-1h3iulQf4TXdHi9__u6WJTQe15l_haZ0';
+    const range = 'Sheet1!A1:B20';
     
-    // Buat instance OAuth2Client
-    const oAuth2Client = new OAuth2Client(clientId, clientSecret, redirectUri);
+    const apiUrl = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${range}?key=${apiKey}`;
 
-    // Buat URL otorisasi dengan scope readonly
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-    });
-
-    console.log('Authorize this app by visiting this url:', authUrl);
-
-    // Setelah pengguna memberikan izin, Anda akan mendapatkan kode otorisasi
-    // yang dapat dimasukkan manual atau dengan cara lain sesuai dengan kebutuhan aplikasi Anda
-    const code = 'CODE_FROM_USER'; // Sesuaikan dengan kode otorisasi yang diterima
-
-    // Tukarkan kode otorisasi dengan token akses
-    oAuth2Client.getToken(code, (err, token) => {
-        if (err) return console.error('Error retrieving access token', err);
-
-        // Token akses sekarang dapat digunakan untuk mengakses Spreadsheet API
-        oAuth2Client.setCredentials(token);
-
-        // Implementasikan logika akses ke Spreadsheet API di sini 
-        const sheets = google.sheets({ version: 'v4', auth: oAuth2Client });
-        sheets.spreadsheets.values.get({
-            spreadsheetId: spreadsheetId,
-            range: 'Sheet1!A1:B20', // Sesuaikan dengan rentang data Anda
-        }, (err, res) => {
-            if (err) return console.error('The API returned an error:', err.message);
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
             const username = document.getElementById('username').value;
             const password = document.getElementById('password').value;
 
-            const userExists = res.data.values.some(row => row[0] === username && row[1] === password);
+            const userExists = data.values.some(row => row[0] === username && row[1] === password);
 
             if (userExists) {
                 alert('Login berhasil!');
@@ -50,6 +24,8 @@ function validateLogin(event) {
             } else {
                 alert('Login gagal. Kredensial tidak valid.');
             }
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
-    });
 }
