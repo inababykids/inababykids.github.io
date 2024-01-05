2ddc46caf466a670c604de32252076b0e11afcf9
inababykids <144520121+inababykids@users.noreply.github.com>
2024-01-05 15:19:42 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,6 @@
 function validateLogin(event) {
-    event.preventDefault(); // Mencegah formulir dikirimkan secara default
+    event.preventDefault(); 
 
-    // Ambil data dari Google Sheets API
     const apiKey = 'AIzaSyBKTs08rp18jacSVRdE1HGpgHAdGyqBXkQ';
     const spreadsheetId = '1F5voYTnm3e-1h3iulQf4TXdHi9__u6WJTQe15l_haZ0';
     const range = 'Sheet1!A1:B20';
@@ -11,20 +10,18 @@ function validateLogin(event) {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Verifikasi kredensial pengguna
             const username = document.getElementById('username').value;
             const password = document.getElementById('password').value;
 
             const userExists = data.values.some(row => row[0] === username && row[1] === password);
 
             if (userExists) {
-                // Pengguna berhasil login
                 alert('Login berhasil!');
                 
-                // Pengalihan halaman
+                localStorage.setItem('sessionToken', 'yourSessionTokenHere');
+
                 window.location.href = '/index.html';
             } else {
-                // Kredensial tidak valid
                 alert('Login gagal. Kredensial tidak valid.');
             }
         })
